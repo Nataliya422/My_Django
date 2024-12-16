8efeade1d14dbc5a68e75f1fb91fb59dd7b03ea0
Nataliya422 <natasavkina310709@yandex.ru>
2024-12-16 21:01:17 +0700

    lesson_55

--- blog/urls.py
+++ blog/urls.py
@@ -17,8 +17,19 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from python_blog.views import main
+from django.urls import include
 
+""""
+str - строки, любые символы кроме слэша '/' (по умолчанию)
+int - положительные целые числа включая 0
+slug - ASCII буквы/цифры, дефисы и подчеркивания
+uuid - уникальные идентификаторы UUID
+path - строки, включая слэши '/'
+"""
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', main),
 ]
+
+# Подключаем python_blog.urls
+path('posts/', include('python_blog.urls'))
