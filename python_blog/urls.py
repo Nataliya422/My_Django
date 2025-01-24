442ca63a322f15d989c148d7cec610a06676d7b9
Nataliya422 <natasavkina310709@yandex.ru>
2025-01-24 13:08:18 +0700

    lesson_54 реверс и навигация

--- python_blog/urls.py
+++ python_blog/urls.py
@@ -3,15 +3,19 @@ from django.urls import path
 from python_blog.views import catalog_posts, post_detail, catalog_categories, category_detail, catalog_tags, tag_detail  
 
 urlpatterns = [
-    path('', catalog_posts),
-    path('<slug:post_slug>/', post_detail),
-
+# Каталог постов
+    path('', catalog_posts, name='posts'),
+    
 # Категории
-    path('categories/', catalog_categories),
-    path('categories/<slug:category_slug>/', category_detail),
+    path('categories/', catalog_categories, name='categories'),
+    path('categories/<slug:category_slug>/', category_detail, name='category_detail'),
 
 # Тэги
-    path('tags/', catalog_tags),
-    path('tags/<slug:tag_slug>/', tag_detail),
+    path('tags/', catalog_tags, name='tags'),
+    path('tags/<slug:tag_slug>/', tag_detail, name='tag_detail'),
+
+# Посты
     
-]
+    path('<slug:post_slug>/', post_detail, name='post_detail'),
+ ]
+    
